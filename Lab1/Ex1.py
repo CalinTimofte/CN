c5a6f888d98485e2cc742608fce2229de67263b7
platicaalex <platicaalex@gmail.com>
2020-02-24 10:17:15 +0200

    Create Ex1.py
    Ex1 made by BO$$ Kalin

---
+++ Lab1/Ex1.py
@@ -0,0 +1,15 @@
+def ex1():
+    u = 1
+    exponent = 0
+    while 1 + u != 1:
+        u = u * (10 ** -1)
+        exponent += 1
+    return u, exponent
+
+
+def ex2():
+    pass
+
+
+def ex3():
+    pass
