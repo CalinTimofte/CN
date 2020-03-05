334d6ce92576e833fb516088e03008edb8468b18
unknown <calin.timofte@yahoo.com>
2020-03-05 18:04:58 +0200

    I tried lol.

--- Lab1/Ex1.py
+++ Lab1/Ex1.py
@@ -1,3 +1,8 @@
+import math
+
+A = [[0,1,0,1],[1,0,0,1]]
+B = [[0,0,0,1],[1,1,1,1]]
+
 def ex1():
     u = 1
     exponent = 0
@@ -16,9 +21,24 @@ def ex2():
     print("Produsul e " + str((xProdus * y) * z == xProdus * (y * z)))
 
 
-def ex3():
-    pass
+def ex3(matrix_A, matrix_B):
+    n = len(matrix_A)
+    m = math.floor(math.log(n)) #nr total de (linii sau coloane)/matrice
+    p = math.ceil(n/m) #nr total de matrici
+
+def slice_matrix_horizontally(B, m, p, n):
+    return_matrixes = []
+    for i in range(p):
+        if i is not p - 1:
+            for j in range(m):
+                    # referinta!!
+                    return_matrixes[i][j] = B[j+i]
+        else:
+            for j in range(m)
 
 
-print(ex1())
-ex2()
+#
+# x = ex1()[0];
+# print((100*x) * x)
+# print(100 * (x * x))
+ex3(A,B)
