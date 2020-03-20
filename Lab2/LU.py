ac4f43a997194a2403b88e725927daa5c876df91
platicaalex <platicaalex@gmail.com>
2020-03-20 14:14:22 +0200

    Update LU.py

--- Lab2/LU.py
+++ Lab2/LU.py
@@ -1,8 +1,9 @@
 import numpy
-# determinare matrici L si U
 import scipy.linalg as la
 
 
+# 1 determinare matrici L si U
+# done, to check
 def descompunereLU(A, n):
     for p in range(n):
         # calculul elementelor liniei p din matricea U
@@ -24,6 +25,8 @@ def descompunereLU(A, n):
     print(A)
 
 
+# 2 determinant A with L, U
+# done, to check
 def determinantALU(A, n):
     detA = 1
     for i in range(n):
@@ -31,6 +34,7 @@ def determinantALU(A, n):
     print(detA)
 
 
+# 3 cautare solutie sistem de ecuatie
 # undone, completat cu cazurile A[i][i], precizia calculelor epsilon
 def calculare_solutieLU(A, n, b):
     y = []
@@ -48,28 +52,71 @@ def calculare_solutieLU(A, n, b):
     return x
 
 
-#to do
-def calcul_norma(A,n,b,x):
+# 4 calculul normei folosind A initiala, b initiala si solutia ecuatiei
+# done, to check
+def calcul_norma(A, n, b, x):
     # a init * x = y
+    y = []
+    for i in range(n):
+        sum = 0
+        for j in range(n):
+            sum += A[i][j] + x[j]
+        y.append(sum)
+
     # y - b init = z
+    for i in range(n):
+        y[i] -= b[i]
+
+    # ||z||
     sum = 0
     for i in range(n):
-        sum += z[i]**2
-    norma = sum ** 1/2
+        sum += y[i] ** 2
+    norma = sum ** 1 / 2
+    return norma
 
 
-#to do
-def inversa_matricii:
+# 5 solutia sistemului si inversa matricii folosind lib
+def done_with_libraries(A, n, b, xLU):
+    xlib = numpy.linalg.solve(A, b)
+    Ainv = numpy.linalg.inv(A)
+    print(numpy.linalg.norm(xLU - xlib))
+    print(numpy.linalg.norm(xLU - numpy.dot(Ainv, b)))
 
-def tema2(A, n, b):
+
+# 6 determinare inversul matricii
+# done, but the result is wrong -> cautare solutie wrong
+def inversa_matricii(A, n):
+    Ainv = [[0 for x in range(n)] for y in range(n)]
+    for i in range(n):
+        e = [0 for i in range(n)]
+        e[i] = 1
+        xstar = calculare_solutieLU(A, n, e)
+        for j in range(n):
+            Ainv[i][j] = xstar[j]
+    Alibinv = numpy.linalg.inv(A)
+    sumlist = []
+    for i in range(n):
+        sum = 0
+        for j in range(n):
+            sum += (Ainv[i][j] - Alibinv[i][j])
+        sumlist.append(sum)
+    C = max(sumlist)
+    return C
+
+
+def tema2(A, b):
+    n = len(A)
+    print(n)
     Ainit = A.copy()
     binit = b.copy()
-    descompunereLU(A, n)
-    determinantALU(A, n)
-    x=calculare_solutieLU(A,n,b)
-    calculare_solutieLU(Ainit, n, binit, x)
+    # descompunereLU(A, n)
+    # determinantALU(A, n)
+    x = calculare_solutieLU(A, n, b)
+    # calcul_norma(Ainit, n, binit, x)
+    done_with_libraries(A, n, b, x)
+    inversa_matricii(A, n)
 
 
 tema2([[2.5, 2, 2],
        [5, 6, 5],
-       [5, 6, 6.5]], 3, [2, 2, 2])
+       [5, 6, 6.5]], [2, 2, 2])
