e935310210841ce1d21045afca5d972802aac8c4
unknown <calin.timofte@yahoo.com>
2020-03-31 19:53:18 +0300

    Done, check it (also should add some comments...).

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -36,6 +36,24 @@ def matrix_sum(a,b):
                 sum_vector[line_num].append(j)
     return sum_vector
 
+def matrix_product(a,b):
+    b_column_matrix = {n: [] for n in range(len(b))}
+    for line_num, i in enumerate(b):
+        for j in i:
+            b_column_matrix[j[1]].append((j[0], line_num))
+    product_matrix = [[] for i in range(len(b))]
+    for line_num, i in enumerate(a):
+        for col_number_b in range(len(b_column_matrix)):
+            product_elem = 0
+            for j in i:
+                for k in b_column_matrix[col_number_b]:
+                    if j[1] == k[1]:
+                        product_elem += k[0] * j[0]
+                        break
+            if product_elem != 0:
+                product_matrix[line_num].append((product_elem, col_number_b))
+    return product_matrix
+
 def compare_vectors(a,b):
     for line_num, i in enumerate(a):
         for j in i:
@@ -52,4 +70,7 @@ a = store_matrix("a.txt")
 b = store_matrix("b.txt")
 sum = matrix_sum(a, b)
 check_sum = store_matrix("sum.txt")
-print(compare_vectors(sum, check_sum))
+prod = matrix_product(a, b)
+check_prod = store_matrix("prod.txt")
+print(prod)
+print(compare_vectors(prod, check_prod))
