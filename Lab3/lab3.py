b7abc11b1e76908ece43a95b163521be6af18152
unknown <calin.timofte@yahoo.com>
2020-03-31 11:39:49 +0300

    Sum function done.

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -1,3 +1,5 @@
+import copy
+
 def store_matrix(filename):
     first = True
     with open(filename, 'r') as matrix:
@@ -11,7 +13,43 @@ def store_matrix(filename):
                 el = float(elements[0][:-1])
                 i = int(elements[1][:-1])
                 j = int(elements[2])
-                vector[i].append((el, j))
-    return (vector)
+                not_found = True
+                for index, tup in enumerate(vector[i]):
+                    if tup[1] == j:
+                        vector[i][index] = (tup[0] + el, tup[1])
+                        not_found = False
+                if not_found:
+                    vector[i].append((el, j))
+    return(vector)
 
-print(store_matrix("a.txt"))
+def matrix_sum(a,b):
+    sum_vector = copy.deepcopy(a)
+    for line_num, i in enumerate(b):
+        for j in i:
+            not_found = True
+            for index, tup in enumerate(sum_vector[line_num]):
+                if tup[1] == j[1]:
+                    sum_vector[line_num][index] = (tup[0] + j[0], tup[1])
+                    not_found = False
+                    break
+            if not_found:
+                sum_vector[line_num].append(j)
+    return sum_vector
+
+def compare_vectors(a,b):
+    for line_num, i in enumerate(a):
+        for j in i:
+            not_found = True
+            for tup in b[line_num]:
+                if tup == j:
+                    not_found = False
+                    break
+            if not_found:
+                return False
+    return True
+
+a = store_matrix("a.txt")
+b = store_matrix("b.txt")
+sum = matrix_sum(a, b)
+check_sum = store_matrix("sum.txt")
+print(compare_vectors(sum, check_sum))
